8de4b425c5087e3407190ac6082fa7ceeb4b4ba6
Ben Yellin <yellinben@gmail.com>
2020-02-06 14:39:15 -0500

    Register shortcuts.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,32 @@
-console.log(`'Allo 'Allo! Content script`)
+'use strict';
+
+import Mousetrap from 'mousetrap';
+import 'mousetrap/plugins/global-bind/mousetrap-global-bind';
+
+(function(window, document) {
+  const simulateClick = elem =>
+    elem.dispatchEvent((
+      new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    })));
+
+  const clickElement = selector => {
+    const elem = document.querySelector(selector);
+    if (!elem) throw new Error(`Could not find matching element: ${selector}`);
+    elem.click();
+  };
+
+  const shortcuts = {
+    ".console-clear-button": "ctrl+l"
+  };
+
+  const registerShortcuts = () => {
+    Object.entries(shortcuts).forEach(([action, keys]) =>
+      Mousetrap.bindGlobal(keys, () => clickElement(action))
+    );
+  };
+
+  registerShortcuts();
+})(window, document);
